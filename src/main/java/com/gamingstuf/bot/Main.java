9741a9cbd9be4a195066809a1693f99c92918681
Wiz_Techno <wiz.techno@nomail.fff>
2018-11-10 18:38:36 -0500

    fififif

--- src/main/java/com/gamingstuf/bot/Main.java
+++ src/main/java/com/gamingstuf/bot/Main.java
@@ -8,11 +8,7 @@ import net.dv8tion.jda.core.entities.Game;
 public class Main {
 	public static void main(String[] args) throws Exception {
 		try {
-<<<<<<< HEAD
 			JDA api = new JDABuilder(AccountType.BOT).setToken("").build();
-=======
-			JDA api = new JDABuilder(AccountType.BOT).setToken("").build();
->>>>>>> e30b5ec514f4b696d7fcc775ae7693764a617d5f
 			api.addEventListener(new GS_Author());
 			api.addEventListener(new GS_Debug());
 			api.addEventListener(new GS_Say());
